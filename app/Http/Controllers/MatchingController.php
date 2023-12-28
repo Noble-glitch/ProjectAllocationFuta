ebd24f045012f765a9061ae2ccc2146c80fc09d3
7j4n1 <ahmaholy@gmail.com>
2023-12-28 15:39:45 +0100

    edit: modify the controller for validation

--- app/Http/Controllers/MatchingController.php
+++ app/Http/Controllers/MatchingController.php
@@ -5,13 +5,106 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Student;
 use App\Models\Supervisor;
+use Illuminate\Support\Facades\Validator;
 
 class MatchingController extends Controller
 {
     //
+    
 
     public function getStudentAndAllocate(Request $request)
     {
+
+        $student = $request->all();
+        $validate = Validator::make($student, [
+            'student_id' => 'required|string',
+            'email' => 'required|string|email',
+            'firstName' => 'required|string',
+            'lastName' => 'required|string',
+            'school' => 'required|string',
+            'department' => 'required|string',
+            'program' => 'required|string',
+            'keywords' => 'required|string',
+            'thesisTitle' => 'required|string',
+            'researchArea' => 'required|string',
+
+        ]);
+
+        if ($validate->fails()) {
+            $errors = $validate->messages();
+            return View('admin', [$errors]);
+        }
+        
+        // $unassignedStudents = Student::where('supervisor_id', null)->get();
+
+        // foreach ($unassignedStudents as $student) {
+        $eligibleSupervisors = Supervisor::has('students', '<', 2)->get();
+
+        $matchingScores = [];
+        foreach ($eligibleSupervisors as $supervisor) {
+            $matchingScore = $this->calculateMatchingScore($request, $supervisor);
+            $matchingScores[$supervisor->id] = $matchingScore;
+        }
+
+        $filteredSupervisors = array_filter($matchingScores, function ($score) {
+            return $score > 0; // Adjust threshold as needed
+        });
+
+        // create new student object
+        $newStudent = new Student();
+        $newStudent->student_id = $student['student_id'];
+        $newStudent->email = $student['email'];
+        $newStudent->firstName = $student['firstName'];
+        $newStudent->middleName = $student['middleName'];
+        $newStudent->lastName = $student['lastName'];
+        $newStudent->school = $student['school'];
+        $newStudent->department = $student['department'];
+        $newStudent->program = $student['program'];
+        $newStudent->keywords = $student['keywords'];
+        $newStudent->thesisTitle = $student['thesisTitle'];
+        $newStudent->researchArea = $student['researchArea'];
+
+        $message = "";
+
+        if ($filteredSupervisors) {
+            $selectedSupervisorId = array_rand($filteredSupervisors);
+            $newStudent->supervisor_id = $selectedSupervisorId;
+            // $newStudent->save();
+            $message .= "Student details saved successfully. <br/>Matching Allocation found";
+        } else {
+            // Handle unmatched student
+            $message .= "Student details saved successfully. <br/>No matching Allocation found";
+        }
+
+        $newStudent->save();
+        
+        return View('admin', compact('message'));
+        
+        // }
+    }
+
+    private function calculateMatchingScore($student, $supervisor)
+    {
+        $studentKeywords = array_merge(
+            explode(',', $student->keywords),
+            explode(',', $student->researchArea),
+            explode(',', $student->thesisTitle)
+        );
+        $supervisorKeywords = array_merge(
+            explode(',', $supervisor->areaExpertise),
+            explode(',', $supervisor->researchArea),
+            explode(',', $supervisor->MajResearchArea),
+            explode(',', $supervisor->keywords),
+            explode(',', $supervisor->MinResearchArea)
+        );
+    
+        // Implement scoring logic (e.g., count shared words with weights)
+        $matchingScore = count(array_intersect($studentKeywords, $supervisorKeywords));
+    
+        return $matchingScore;
+    }
+
+    public function allocateAutomatically(){
         $unassignedStudents = Student::where('supervisor_id', null)->get();
 
         foreach ($unassignedStudents as $student) {
@@ -35,26 +128,5 @@ class MatchingController extends Controller
                 // Handle unmatched student using strategies mentioned earlier
             }
         }
-    }
-
-    private function calculateMatchingScore($student, $supervisor)
-    {
-        $studentKeywords = array_merge(
-            explode(',', $student->keywords),
-            explode(',', $student->researchArea),
-            explode(',', $student->thesisTitle)
-        );
-        $supervisorKeywords = array_merge(
-            explode(',', $supervisor->areaExpertise),
-            explode(',', $supervisor->researchArea),
-            explode(',', $supervisor->MajResearchArea),
-            explode(',', $supervisor->keywords),
-            explode(',', $supervisor->MinResearchArea)
-        );
-    
-        // Implement scoring logic (e.g., count shared words with weights)
-        $matchingScore = count(array_intersect($studentKeywords, $supervisorKeywords));
-    
-        return $matchingScore;
     }
 }
